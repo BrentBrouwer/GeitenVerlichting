993c17b3a65044ccd3a39b9ef6c422c0e929b612
Brent Brouwer <brentbrouwer13@gmail.com>
2024-07-14 17:05:26 +0200

    Added images to the html page. Status led is working

--- GeitenVerlichting/src/main.cpp
+++ GeitenVerlichting/src/main.cpp
@@ -1,24 +1,45 @@
 #include <Arduino.h>
 #include "WiFi.h"
-#include "json_parser.h"
+#include <ESPmDNS.h>
 
+#define StatusPin 2
 #define LedPin 5
 
+
 #pragma region Properties
+// LED Status
+unsigned long m_LastStateChange = 0;
+const unsigned long m_StateChangeTime = 1000; 
+
 // Wifi Server
 WiFiServer m_WifiServer;
 uint16_t m_PortNr = 80; // Default port number for HTTP web sockets, 443 for HTTPS
 const char *m_SSID = "vBakel";
 const char *m_Password = "1001100111";
+const char *m_DomainName = "geitjes";
 
 // Wifi Client
 const long m_TimeOut = 2000;
 unsigned long m_CurrentTime = millis();
 unsigned long m_ConnectionTime = 0;
 
-// Messages
+// HTTP
 const char * LedOn = "GET /LED/ON";
 const char * LedOff = "GET /LED/OFF";
+
+const String html_DocType = "<!doctype html>";
+const String html_Html = "<html lang=\"en-US\">";
+const String html_HtmlEnd = "</html>";
+
+const String html_Head = "<head><title>Geitjes</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<link rel=\"icon\" href=\"data:,\">";
+const String html_HeadEnd = "</head>";
+
+const String html_Style = "<style>";
+const char* html_ImageSourceHappy = "https://i.pinimg.com/originals/3a/ee/8d/3aee8d3cf15482a8972195ef398a081a.jpg";
+const char* html_ImageSourceSad = "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/sad-goat-ezume-images.jpg";
+const String html_StyleEnd = "</style>";
+
+const String html_BodyEnd = "</body>";
 
 struct ClientMsg
 {
@@ -29,18 +50,6 @@ struct ClientMsg
 #pragma endregion
 
 #pragma region HTML Page
-const String html_DocType = "<!doctype html>";
-const String html_Html = "<html lang=\"en-US\">";
-const String html_HtmlEnd = "</html>";
-
-const String html_Head = "<head><title>Geitjes</title><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<link rel=\"icon\" href=\"data:,\">";
-const String html_HeadEnd = "</head>";
-
-const String html_Style = "<style>";
-const String html_StyleEnd = "</style>";
-
-const String html_BodyEnd = "</body>";
-
 void CreateHtmlPage(WiFiClient client)
 {
     bool ledOn = digitalRead(LedPin);
@@ -51,7 +60,17 @@ void CreateHtmlPage(WiFiClient client)
     // Head-----------------------------------------------------
     // Header
     client.println(html_Head);
-    client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+    client.println(html_Style);
+
+    // Background image
+    client.printf("body {background-image: url('%s');\n", ledOn ? html_ImageSourceHappy : html_ImageSourceSad);
+    client.println("background-size: cover;");
+    client.println("background-repeat: none;}");
+
+    // Text Color
+    client.printf("h1 {color: %s}\n", ledOn ? "green" : "red");
+
+    client.println("html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
     client.printf(".button { background-color: %s; border: 2px solid black; color: black; padding: 16px 40px;\n", ledOn ? "red" : "green");
     client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
     client.println(html_StyleEnd);
@@ -90,9 +109,31 @@ void InitSerialMonitor()
     Serial.println("Serial monitor opened");
 }
 
+void HandleLedStatus()
+{
+    unsigned long currentTime = millis();
+
+    if ((currentTime - m_LastStateChange) > m_StateChangeTime)
+    {
+        m_LastStateChange = currentTime;
+        if (digitalRead(StatusPin))
+        {
+            digitalWrite(StatusPin, LOW);
+        }
+        else
+        {
+            digitalWrite(StatusPin, HIGH);
+        }
+        
+        // Serial.printf("Status pin: %s\n", digitalRead(StatusPin) ? "on" : "off");
+    }
+}
+
 #pragma region Outputs
 void InitOutputs()
 {
+    pinMode(StatusPin, OUTPUT);
+    digitalWrite(StatusPin, LOW);
     pinMode(LedPin, OUTPUT);
     digitalWrite(LedPin, LOW);
 
@@ -101,6 +142,8 @@ void InitOutputs()
 
 bool ControlOutputs(String msg)
 {
+    bool ledOn = digitalRead(LedPin);
+
     if (msg.indexOf(LedOn) >= 0)
     {
         digitalWrite(LedPin, HIGH);
@@ -121,6 +164,23 @@ bool ControlOutputs(String msg)
 #pragma region WiFi
 void InitWifi()
 {
+    // Set the Local Domain Name
+    mdns_init();
+    mdns_hostname_set(m_DomainName);
+    mdns_instance_name_set(m_DomainName);
+    Serial.printf("MDNS responder started at http://%s.local\n", m_DomainName);
+
+    // Hostname
+    if (WiFi.mode(WIFI_STA) && WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE) && WiFi.setHostname(m_DomainName))
+    {
+        Serial.print("Host name: ");
+        Serial.println(WiFi.getHostname());
+    }
+    else
+    {
+        Serial.println("Failed to set the host name");
+    }
+
     // Connect to the local Network
     Serial.printf("Connect to %s\n", m_SSID);
     WiFi.begin(m_SSID, m_Password);
@@ -192,6 +252,8 @@ void setup()
 
 void loop()
 {
+    HandleLedStatus();
+
     // Read incoming clients
     ClientMsg clientData = GetMessage();
 
@@ -218,8 +280,6 @@ void loop()
         // Close the Connection
         clientData.client.stop();
         Serial.println("Client disconnected");
-    }    
-
-    delay(1000);
+    }
 }
 #pragma endregion
